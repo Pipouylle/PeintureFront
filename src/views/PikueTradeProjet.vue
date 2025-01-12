cd62e3b9433e06fca97c21fae57160504191bbee
merloj <johan.merlo@etu.umontpellier.fr>
2025-01-12 18:38:40 +0100

    refractor some files

--- src/views/PikueTradeProjet.vue
+++ src/views/PikueTradeProjet.vue
@@ -1,17 +1,34 @@
 <script lang="ts">
 import {Vue, Component} from 'vue-facing-decorator';
+import NavBar from "@/component/common/NavBar.vue";
 
-@Component({})
+@Component({
+  components: {NavBar}
+})
 export default class PikueTradeProjet extends Vue {
 
 }
 </script>
 
 <template>
+  <div>
+    <NavBar/>
+    <div class="content">
+
+    </div>
+  </div>
+
 
 
 </template>
 
 <style scoped>
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 80%;
+  margin: 0 auto;
+}
 
 </style>
